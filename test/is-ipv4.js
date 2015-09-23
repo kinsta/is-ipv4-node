496d355234762a18b2332af7ba426bf590ca8118
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-09-23 09:02:40 +0200

    Replace " with ' in definitions of all strings in is-ipv4.js tests

--- test/is-ipv4.js
+++ test/is-ipv4.js
@@ -1,14 +1,14 @@
-var assert = require("assert");
-var isIPv4 = require("../lib/is-ipv4");
+var assert = require('assert');
+var isIPv4 = require('../lib/is-ipv4');
 
-describe("is-ipv4", function() {
-    describe("init", function() {
-        it("should return true if String is an IPv4 address", function() {
-            assert.equal(isIPv4("46.19.37.108"), true, "String should be an IPv4 address");
+describe('is-ipv4', function() {
+    describe('init', function() {
+        it('should return true if String is an IPv4 address', function() {
+            assert.equal(isIPv4('46.19.37.108'), true, 'String should be an IPv4 address');
         });
 
-        it("should return false if String is not an IPv4 address", function() {
-            assert.equal(isIPv4("Hello World!"), false, "String should not be an IPv4 address");
+        it('should return false if String is not an IPv4 address', function() {
+            assert.equal(isIPv4('Hello World!'), false, 'String should not be an IPv4 address');
         });
     });
 });
