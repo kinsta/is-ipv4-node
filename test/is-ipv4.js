0e3d9e337b4bb534e846bce03e512ae31c0ac6f9
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-10-21 12:36:26 +0200

    Fix anonymous function declaration coding style in is-ipv4.js tests

--- test/is-ipv4.js
+++ test/is-ipv4.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 var isIPv4 = require('../lib/is-ipv4');
 
-describe('is-ipv4', function() {
+describe('is-ipv4', function () {
     describe('init', function() {
         it('should return true if String is an IPv4 address', function() {
             assert.equal(isIPv4('46.19.37.108'), true, 'String should be an IPv4 address');
